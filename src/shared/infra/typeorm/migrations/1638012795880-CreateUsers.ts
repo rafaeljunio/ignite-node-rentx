bb1a6899432c218ff4bb40e96587870efb3563f2
Rafael Junio <rafaeljunio7@gmail.com>
2021-12-09 18:29:42 -0300

    Implementation of the module Rental

--- src/shared/infra/typeorm/migrations/1638012795880-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1638012795880-CreateUsers.ts
@@ -1,54 +1,53 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateUsers1638012795880 implements MigrationInterface {
-
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "users",
+        name: 'users',
         columns: [
           {
-            name: "id",
-            type: "uuid"
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
           },
           {
-            name: "name",
-            type: "varchar"
+            name: 'name',
+            type: 'varchar',
           },
           {
-            name: "username",
-            type: "varchar",
-            isUnique: true
+            name: 'username',
+            type: 'varchar',
+            isUnique: true,
           },
           {
-            name: "password",
-            type: "varchar"
+            name: 'password',
+            type: 'varchar',
           },
           {
-            name: "email",
-            type: "varchar"
+            name: 'email',
+            type: 'varchar',
           },
           {
-            name: "driver_license",
-            type: "varchar"
+            name: 'driver_license',
+            type: 'varchar',
           },
           {
-            name: "isAdmin",
-            type: "boolean",
-            default: false
+            name: 'isAdmin',
+            type: 'boolean',
+            default: false,
           },
           {
-            name: "created_at",
-            type: "timestamp",
-            default: "now()"
-          }
-        ]
-      })
-    )
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("users")
+    await queryRunner.dropTable('users');
   }
-
 }
